07f10304e3ddccc425032e925b667f6930dc3f29
Syrgak <Syrgak21@gmail.com>
2021-01-13 12:23:56 +0600

    ServiceLine

---
+++ main.js
@@ -0,0 +1,18 @@
+let d = document
+let menuButton = d.getElementById('hamburger'),
+    menu = d.getElementById('menu')
+
+menuButton.addEventListener('click', () => {
+  menuButton.classList.toggle('active')
+  menu.classList.toggle('active')
+})
+
+$('.slogan_list').slick({
+  infinite: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  fade: true,
+  speed: 1000,
+});
